8a38890a8a1a92b118c83f87b28978480b70e875
Esalencia <esalenciar@gmail.com>
2022-07-02 14:19:51 -0700

    printing numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Prints all single digit numbers of base 16 starting from 0.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+int d = '0';
+
+while (d <= '15')
+{
+putchar(d);
+d++;
+}
+putchar('\n');
+return (0);
+}
