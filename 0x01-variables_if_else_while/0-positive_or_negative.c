2ec9766dfd89e89bb985bbcf2e76479be5c93891
Esalencia <esalenciar@gmail.com>
2022-07-02 08:45:18 -0700

    Prints the last digit of the number stored in the variable n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,11 +3,11 @@
 #include <stdio.h>
 
 /**
- * main - Prints whether the number stored in the variable n 
- * is positive, negative, or zero.
- *
- * Return: Always 0 (success)
- */
+* main - Prints whether the number stored in the variable n
+* is positive, negative, or zero.
+*
+* Return: Always 0 (success)
+*/
 int main(void)
 {
 int n;
