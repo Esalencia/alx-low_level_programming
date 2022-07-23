7f344024115fca40431050c1b5ccc3a18144710c
Esalencia <esalenciar@gmail.com>
2022-07-23 09:10:34 -0700

    recursion in c.alx

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - searchs for the factorial of a number
+ * @n: the number to find the factorial of
+ *
+ * Return: the factorial of the number
+ */
+
+int factorial(int n)
+{
+	int new;
+
+	if (n == 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	new = factorial(n - 1);
+	return (n * new);
+}
