970cb5b108af5d1aa0395518feb8d92b2db8b27a
Esalencia <esalenciar@gmail.com>
2022-08-03 15:00:31 -0700

    mandatory malloc fre task

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,30 +1,24 @@
-#include <stdlib.h>
 #include "main.h"
 
 /**
-* array_range - function that creates an array of integers
-* @min: the first array value
-* @max: the second array value
-*
-* Return: pointer to the new array
-*/
+ * array_range - creates an array of integers
+ * @min: minimum
+ * @max: maximum
+ * Return: array
+ */
+
 int *array_range(int min, int max)
 {
-int *prt;
-int i, size;
+	int *arr, i = 0, t = min;
 
-if (min > max
-return (NULL);
-    
-size = max - min + 1;
-    
-prt = malloc(sizeof(int) * size);
-    
-if (prt == NULL)
-return (NULL);
-    
-for (i = 0; min <= max; i++)
-prt[i] =min--;
-    
-return (prt);
+	if (min > max)
+		return (0);
+	arr = malloc((max - min + 1) * sizeof(int));
+
+	if (!arr)
+		return (0);
+	while (i <= max - min)
+		arr[i++] = t++;
+
+	return (arr);
 }
