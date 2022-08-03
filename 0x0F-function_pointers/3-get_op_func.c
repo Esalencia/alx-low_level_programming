e76d6ed25745d674df770d09bf3844b2cea961ce
Esalencia <esalenciar@gmail.com>
2022-08-03 16:31:02 -0700

    function pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
+
 /**
- * get_op_func - identify function
- * @s: funct
- * Return: int
- **/
+ * get_op_func - function pointer that selects the correct function to perform
+ * the operation asked by the user
+ * @s: the operator given by the user
+ *
+ * Return: pointer to the function that corresponds to the
+ * operator given as a parameter
+ */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
@@ -16,18 +18,16 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int j;
+	int i;
 
-	j = 0;
-	while (j < 5)
+	i = 0;
+
+	while (ops[i].op)
 	{
-		if (s[0] == ops[i].op[0])
-		{
-			return (ops[j].f);
-		}
-		j++;
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
 	}
-
 
 	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,12 +4,13 @@
  * main - program that perfroms simple operations
  * @argc: number of arguments
  * @argv: array of arguments
+ *
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int argA, argB, result;
-	char x;
+	int arg1, arg2, result;
+	char o;
 	int (*func)(int, int);
 
 	if (argc != 4)
@@ -18,8 +19,8 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	argA = atoi(argv[1]);
-	argB = atoi(argv[3]);
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
 
 	func = get_op_func(argv[2]);
 
@@ -29,15 +30,15 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	x = *argv[2];
+	o = *argv[2];
 
-	if ((x == '/' || x == '%') && argB == 0)
+	if ((o == '/' || o == '%') && arg2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	result = func(argA, argB);
+	result = func(arg1, arg2);
 
 	printf("%d\n", result);
 
